8faf129147e55482738c9dd6cd3502e4d78ad48c
Trung Phan <trung.phanchi@gmail.com>
2013-03-27 17:52:35 -0500

    Ref

---
+++ src/main/java/net/tp/algo/util/Ref.java
@@ -0,0 +1,15 @@
+package net.tp.algo.util;
+
+import net.tp.algo.tree.Heap;
+
+/**
+ * 
+ * Pointer to some internal structure. It's in heap to give client an option to do some operations quickly.
+ * 
+ * @see Heap#remove(Ref)
+ * @author Trung Phan
+ *
+ */
+public interface Ref {
+
+}
