bfcb56c87bb8bf8d9afe56dc904193d123d66f8f
Trung Phan <trung.phanchi@gmail.com>
2013-03-28 01:11:44 -0500

    Ref

--- src/main/java/net/tp/algo/util/Ref.java
+++ src/main/java/net/tp/algo/util/Ref.java
@@ -1,15 +1,17 @@
 package net.tp.algo.util;
 
-import net.tp.algo.tree.Heap;
+import net.tp.algo.tree.BinaryHeap;
 
 /**
  * 
  * Pointer to some internal structure. It's in heap to give client an option to do some operations quickly.
  * 
- * @see Heap#remove(Ref)
+ * @see BinaryHeap#remove(Ref)
  * @author Trung Phan
  *
  */
 public interface Ref {
 
+	public boolean valid();
+	
 }
